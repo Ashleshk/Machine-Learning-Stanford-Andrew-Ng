689c5c48e15b776568cf65507e924afe0d0382d7
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-29 14:13:29 +0530

    Week-02 Material

---
+++ Week-2/machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -0,0 +1,15 @@
+function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+    theta = theta - alpha * (1/m) * (((X*theta) - y)' * X)'; % Vectorized  
+    J_history(iter) = computeCostMulti(X, y, theta);
+end
+
+end
